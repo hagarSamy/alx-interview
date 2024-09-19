2d513937773dacee730a5bd654597e632adc6c37
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-20 01:50:16 +0300

    coinChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,16 +5,17 @@ makeChange
 
 
 def makeChange(coins, total):
-    '''determine the fewest number of coins needed to meet a given amount total.'''
+    '''determine the fewest number of coins needed to
+    meet a given amount total.'''
     if total <= 0:
         return 0
     from collections import deque
 
     # Sort the coins in descending order to try the largest coins first
     coins.sort(reverse=True)
-    
+
     count = 0  # To count the number of coins used
-    
+
     for coin in coins:
         if total == 0:
             break
@@ -23,6 +24,6 @@ def makeChange(coins, total):
             num_coins = total // coin
             count += num_coins
             total -= num_coins * coin
-    
+
     # If we can't reach 0, return -1
     return count if total == 0 else -1
